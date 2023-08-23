5f021dd2fba7ca07707979744a61c65a4ebcbd38
Goopalamoorthy <142672172+Goopalamoorthy@users.noreply.github.com>
2023-08-23 23:45:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -14,24 +14,24 @@ class script(object):
     HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://t.me/QTVS_BOT_X_CLOUD')
     START_TXT = environ.get("START_TXT", '''<b>ʜᴇʏ... {}
     
-ᴍʏ ɴᴀᴍᴇ ɪs <a href=http://t.me/Binthu_x_robot><b>ᴍᴀᴅᴀɴ ᴏᴘ x ꜰɪʟᴛᴇʀ ʙᴏᴛ</b></a>
+ᴍʏ ɴᴀᴍᴇ ɪs <a href=http://t.me/Binthu_x_robot><b>𝕋𝕖𝕤𝕤𝕒 𝕏 𝔽𝕚𝕝𝕥𝕖𝕣</b></a>
 
 ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇs/sᴇʀɪᴇs ᴀɴᴅ ʟᴏᴛ ᴍᴏʀᴇ... ᴅᴏɴ'ᴛ ᴡᴀsᴛᴇ ʏᴏᴜʀ ᴛɪᴍᴇ ʙʏ ʟᴏᴏᴋɪɴɢ ᴛᴏ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ.. ᴀᴅᴅ & ᴇɴᴊᴏʏ ᴍᴇʜ ᴘᴏᴡᴇʀ
 
-ᴍᴀᴅᴇ ᴡɪᴛʜ ❤️ ʙʏ <a href=https://t.me/SMD_Owner><b>𝗠𝗔𝗗𝗛𝗔𝗡 𝗢𝗣</b></a></b>''')
+ᴍᴀᴅᴇ ᴡɪᴛʜ ❤️ ʙʏ <a href=https://t.me/SMD_Owner><b>𝕊𝕄𝔻_𝕆𝕨𝕟𝕖𝕣</b></a></b>''')
     HELP_TXT = """ʜᴇʏ {}
 ʜᴇʀᴇ ɪꜱ ᴍʏ ᴄᴏᴍᴍᴀɴᴅꜱ."""
     ABOUT_TXT = """<b>
-╭────[ About Meh ]────⍟
+╭────[ ℚ𝕋𝕍𝕊𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃 ]────⍟
 │
-├⍟ Meh Name : <a href=http://t.me/Binthu_x_robot><b>𝗞𝗔𝗠𝗡𝗔𝗔𝗧𝗜</b></a>
-├⍟ Owner : <a href=https://t.me/SMD_Owner><b>𝗥𝗮𝘁𝗵𝗶𝗻𝗮𝘃𝗲𝗹</b></a>
-├⍟ Version : Dubai Kaaran
-├⍟ Server : <a href=https://t.me/SMD_Owner><b>𝗣𝗼𝗿𝘂𝗸𝗶</b></a>
-├⍟ Language : ᴘʏᴛʜᴏɴ 3
-├⍟ Framework : ᴘʏʀᴏɢʀᴀᴍ
-├⍟ Developer : <a href=https://t.me/SMD_Owner><b>𝗠𝗮𝗻𝗶𝗺𝗮𝗮𝗿𝗮𝗻</b></a>
-├⍟ Powered By  : <a href=https://t.me/SMD_Owner><b>𝗣𝗮𝗻𝗱𝗶𝘆𝗮𝗻</b></a>
+├⍟ 𝕆𝕌ℝ 𝔹𝕆𝕋 ℕ𝔸𝕄𝔼 : <a href=http://t.me/Binthu_x_robot><b>𝗞𝗔𝗠𝗡𝗔𝗔𝗧𝗜</b></a>
+├⍟ 𝔸𝕌𝕋ℍ𝕆ℝ : <a href=https://t.me/SMD_Owner><b>𝔸.𝕂 🍁</b></a>
+├⍟ ℙ𝔸𝔼ℂ𝔼 : <a href=https://t.me/SMD_Owner><b>𝕋ℍ𝔸𝕃𝔸 𝔸𝕁𝕀𝕋ℍ🦞</b></a>
+├⍟ ℙℝ𝕆𝕁𝔼ℂ𝕋 : <a href=https://t.me/SMD_Owner><b>𝕌𝕃𝕋𝕀𝕄𝔼𝕋 𝕊𝕋𝔸ℝ❤‍🔥</b></a>
+├⍟ 𝕃𝔸ℕ𝔾𝕌𝔸𝔾𝔼 : <a href=https://t.me/SMD_Owner><b>ℕ𝔸𝕐𝔸ℕ𝔻𝔸ℝ𝔸☠️</b></a>
+├⍟ 𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 : <a href=https://t.me/SMD_Owner><b>ℍ𝔸ℕ𝕌𝕊ℍ𝕀𝕂𝔸😈</b></a>
+├⍟ ℂℝ𝔼𝔸𝕋𝕆ℝ : <a href=https://t.me/SMD_Owner><b>𝕊𝔸𝕄𝔸ℕ𝕋ℍ𝔸🥴</b></a>
+├⍟ 𝕊𝔼ℝ𝕍𝕀ℂ𝔼 ℍ𝕌𝔹  : <a href=https://t.me/SMD_Owner><b>𝕋𝔸𝕄𝔸ℕ𝔸😊</b></a>
 │
 ╰─────────────────────⍟<b>"""
     SOURCE_TXT = """<b>𝐂𝐫𝐞𝐚𝐭𝐞 𝐎𝐧𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬 💗
