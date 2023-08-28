08f921fb2b65e7f797cb6d6fbebc9930e745d976
Goopalamoorthy <142672172+Goopalamoorthy@users.noreply.github.com>
2023-08-28 10:09:47 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -57,9 +57,9 @@ async def save_group(bot, message):
                 video="https://telegra.ph/file/03691465baa774e46506d.mp4",                                               
                                                  caption=f'<b>Hey there... {u.mention} 😍\nWelcome to {message.chat.title} How Are You?\n\n👉Request Format\nMovie Name (Correct spelling)\nYear Or Language\n\n👉 Search On Google & Paste Here! With Correct Proper Spelling! To Get Movie\n\n👉Example :\nThunivu (2023) Tamil\nVarisu (2023) Tamil\n\n👉Dont Type ( Movie, bro, anupuga, hd) You wont get Anything!</b>',
 
-                                                 reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔥  ↭ Main Channel ↭  🔥', url='https://t.me/qtmve_linkzZ') ],
+                                                 reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔥  ↭ Main Channel ↭  🔥', url='https://t.me/QTVS_BOT_X_CLOUD') ],
 
-                                                                                      [ InlineKeyboardButton('🔥  ↭ Request Channel ↭  🔥', url='https://t.me/+5zdnXfOqEEwxNjQ1') ]
+                                                                                      [ InlineKeyboardButton('🔥  ↭ Request Channel ↭  🔥', url='https://t.me/QTVS_BOT_X_CLOUD') ]
 
                                                                                     ] )
 
